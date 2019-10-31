641ed9ed338fa89e2374e96592956ef139bfaeca
RBAMOUSER\sanspenj <jordi.sans@roche.com>
2019-10-31 08:58:55 +0100

    remove redundant conversion to list

--- 2015/python/day1/day1.py
+++ 2015/python/day1/day1.py
@@ -7,7 +7,7 @@ def part1(data):
 
 def part2(data):
     floor = 0
-    for index, movement in list(enumerate(data)):
+    for index, movement in enumerate(data):
         floor = floor + 1 if movement == "(" else floor - 1
         if (floor == -1):
             print(index + 1)
@@ -16,4 +16,5 @@ def part2(data):
 f = open("input.txt", "r")
 
 data = f.read()
+part1(data)
 part2(data)
