bdfe25f559e594f8e2b8dcb2c1e265dc75368c30
jordi <jordi.sans@roche.com>
2020-12-01 17:51:13 +0100

    add input day1 2020. read it

--- 2020/day1/main.go
+++ 2020/day1/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
-    "fmt"
+	"bufio"
+	"fmt"
+	"os"
 )
 
-
 func main() {
-    fmt.Println("Advent of Code 2020 day 1")
+	fmt.Println("Advent of Code 2020 day 1")
+	f, _ := os.Open("input")
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
 }
